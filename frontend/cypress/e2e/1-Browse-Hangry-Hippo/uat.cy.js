cb6ef517b040f7b9a907e198f1c102a314d8b7df
Laurens <goormachtigh.laurens@gmail.com>
2025-06-22 12:41:42 +0700

    cypress

--- frontend/cypress/e2e/1-Browse-Hangry-Hippo/uat.cy.js
+++ frontend/cypress/e2e/1-Browse-Hangry-Hippo/uat.cy.js
@@ -3,9 +3,17 @@ describe('Shows Appeteasers', () => {
     cy.visit('http://hangryweb-env.eba-vdyerazm.us-east-1.elasticbeanstalk.com/');
   });
   it('selects Handhelds', () => {
+    cy.contains('Handhelds').click();
+    cy.contains('Cheese Burger');
+    cy.contains('Fajita Tacos');
     //TODO: Add a test to check Handhelds
   });
   it('selects Appeteasers', () => {
+    cy.contains('Appeteasers').click();
+    cy.contains('Tater Tots');
+    cy.contains('Buffalo Wings');
+    cy.contains('Cheese Burger').should('not.exist');
+    cy.contains('Fajita Tacos').should('not.exist');
     //TODO: Add a test to check Appeteasers
   });
 });
